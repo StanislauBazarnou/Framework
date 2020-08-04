13f032b096df23d4a1202126b5440d252d5f79d2
Stanislau Bazarnou <karasik026@gmail.com>
2020-08-04 21:48:29 +0300

    Revert "trying start jenkins, changed some chrome settings"
    This reverts commit 22ce0bf453779cdfeb983fdd35852999491ee46a.

--- src/test/java/CommonConditions.java
+++ src/test/java/CommonConditions.java
@@ -1,7 +1,6 @@
 import framework.util.TestListener;
 import framework.driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
@@ -13,21 +12,6 @@ public class CommonConditions {
     @BeforeMethod()
     public void setUp() {
         driver = DriverSingleton.getDriver();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
-        options.addArguments("--headless");
-        options.setExperimentalOption("useAutomationExtension", false);
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("start-maximized"); // open Browser in maximized mode
-//        options.addArguments("disable-infobars"); // disabling infobars
-//        options.addArguments("--disable-extensions"); // disabling extensions
-//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//        options.addArguments("--no-sandbox"); // Bypass OS security model
-//        options.addArguments("--headless");
     }
 
     @AfterMethod(alwaysRun = true)
